e6c5be13a5b0cb0e784d7b59a3e2f48302cb9109
bino <ska3287776@naver.com>
2023-05-20 22:29:39 +0900

    Update DB 4강 시험처리 2.sql

--- 데이터베이스프로그래밍 SQL쿼리/DB 4강 시험처리 2.sql
+++ 데이터베이스프로그래밍 SQL쿼리/DB 4강 시험처리 2.sql
@@ -58,30 +58,3 @@ call insert_test(3, 1000);
 select * from testing order by stu_id;
 select * from testing;
 select count(*) from testing;
-
--- 채점하기
-insert into scoring select 
-t.subjectID, t.stu_name, t.stu_id,
-(a.a01 = t.a01) as s01,
-(a.a02 = t.a02) as s02,
-(a.a03 = t.a03) as s03,
-(a.a04 = t.a04) as s04,
-(a.a05 = t.a05) as s05,
-(a.a06 = t.a06) as s06,
-(a.a07 = t.a07) as s07,
-(a.a08 = t.a08) as s08,
-(a.a09 = t.a09) as s09,
-(a.a10 = t.a10) as s10,
-(a.a11 = t.a11) as s11,
-(a.a12 = t.a12) as s12,
-(a.a13 = t.a13) as s13,
-(a.a14 = t.a14) as s14,
-(a.a15 = t.a15) as s15,
-(a.a16 = t.a16) as s16,
-(a.a17 = t.a17) as s17,
-(a.a18 = t.a18) as s18,
-(a.a19 = t.a19) as s19,
-(a.a20 = t.a20) as s20,
-s01 + s02 + s03 + s04 + s05 + s06 + s07 + s08 + s09 + s10 +
-s11 + s12 + s13 + s14 + s15 + s16 + s07 + s08 + s09 + s10
-from answer as a,testing as t where a.subjectID = t.subjectID;
