d434628c544bb3b6ea05dc8279c70984a420f9ff
Nam Jeong-Hyeon <ska3287776@naver.com>
2023-05-23 06:50:35 +0900

    Create DB 4강 리조트.sql

---
+++ 데이터베이스프로그래밍 SQL쿼리/DB 4강 리조트.sql
@@ -0,0 +1,9 @@
+use mysql;
+create table reservation(
+name varchar(5), reserve_date date,
+room int, addr varchar(5),
+tel varchar(20), ipgum_name varchar(5),
+memo varchar(20), input_date date);
+desc reservation;
+
+ 
