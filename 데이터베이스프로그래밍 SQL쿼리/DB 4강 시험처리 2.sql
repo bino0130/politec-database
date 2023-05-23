74682df58d0af783599064f825e5ac376c6895d1
Nam Jeong-Hyeon <ska3287776@naver.com>
2023-05-24 04:23:34 +0900

    Update DB 4강 시험처리 2.sql

--- 데이터베이스프로그래밍 SQL쿼리/DB 4강 시험처리 2.sql
+++ 데이터베이스프로그래밍 SQL쿼리/DB 4강 시험처리 2.sql
@@ -16,45 +16,46 @@ rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1);
 select * from answer;
 
 -- 시험 테이블 데이터 입력
-drop procedure if exists insert_test;
+drop procedure if exists insert_test; # 있으면 지워라
 delimiter $$
-create procedure insert_test(_subid integer, _last integer)
-begin
-declare _name varchar(20);
-declare _id integer;
-declare _cnt1 integer;
-declare _cnt2 integer;
-set _cnt1 = 0;
+create procedure insert_test(_subid integer, _last integer) # insert_test 프로시저 생성
+begin # 시작
+declare _name varchar(20); # varchar(20) _name 변수 생성
+declare _id integer; # integer _id 변수 생성
+declare _cnt1 integer; # integer _cnt1 변수 생성 -> subjectID에 사용
+declare _cnt2 integer; # integer _cnt2 변수 생성
+set _cnt1 = 0; # _cnt1 = 0으로 초기화
 
-delete from testing;
-_loop1 : loop
-set _cnt1 = _cnt1 + 1;
-set _cnt2 = 0;
-		_loop2 : loop
-			set _cnt2 = _cnt2 + 1;
+delete from testing; # 시험 테이블 전체 데이터 삭제
+_loop1 : loop # 반복문 1
+set _cnt1 = _cnt1 + 1; # cnt1++
+set _cnt2 = 0; # cnt2 = 0으로 초기화
+		_loop2 : loop # 반복문 2
+			set _cnt2 = _cnt2 + 1; # _cnt2++
 			set _name = concat("학번", cast(_cnt2 as char(4)));
-			set _id = _cnt2;
-			insert into testing value (_cnt1, _name, _id, 
-			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1,
-			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1,
-			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1,
-			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1);
+            # concat : 2개 이상의 문자열을 합치는 함수
+			# cast : 데이터 타입을 바꿔주는 함수 (= 형변환 함수)
+			set _id = _cnt2; # _id = _cnt2으로 설정
+			insert into testing value (_cnt1, _name, _id, # subjectID, stu_name, stu_id
+			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, # a01~a05 성적
+			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, # a06~a10 성적
+			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, # a11~a15 성적
+			rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1, rand()*3 + 1); # a16~a20 성적
 			
-			if _cnt2 = _last then
-				leave _loop2;
-			end if;
-		end loop;
+			if _cnt2 = _last then # _cnt2가 입력받은 숫자와 같으면
+				leave _loop2; # break
+			end if; # if 종료
+		end loop; # 반복문 종료
 
-if _cnt1 = _subid then
-	leave _loop1;
-end if;
-end loop;
+if _cnt1 = _subid then # _cnt1이 _subid와 같으면
+	leave _loop1; # break
+end if; # if 종료
+end loop; # 반복문 종료
 
-end $$
+end $$ # delimiter 종료
 delimiter ;
 
-call insert_test(3, 1000);
+call insert_test(3, 1000); # insert_test에 3,1000 입력
 
-select * from testing order by stu_id;
-select * from testing;
-select count(*) from testing;
+select * from testing; # 시험 테이블 출력
+select count(*) from testing; # 시험 테이블 데이터 개수 출력
