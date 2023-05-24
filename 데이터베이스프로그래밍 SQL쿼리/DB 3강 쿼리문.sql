41e7e00eaf843651291505a7f6fbc56388aecb9f
bino <ska3287776@naver.com>
2023-05-24 20:49:45 +0900

    Update DB 3강 쿼리문.sql
    DB 3강 실습 쿼리 업데이트

--- 데이터베이스프로그래밍 SQL쿼리/DB 3강 쿼리문.sql
+++ 데이터베이스프로그래밍 SQL쿼리/DB 3강 쿼리문.sql
@@ -84,11 +84,12 @@ select eng, count(eng) from examtable group by eng having count(eng)>1;
 # group by절에서는 where 대신 having을 사용해야 한다.
 
 -- 슬라이드 9 실습
-create table voteTable(
-name varchar(5),
-age int);
-drop table if exists voteTable;
+drop table if exists voteTable; # 있으면 지워라
+create table voteTable( # voteTable 생성
+name varchar(5), # varchar(5) name
+age int);  # int age
 
+# 트와이스 데이터 입력
 insert into voteTable value ("나연", (floor(rand()*9) + 1) * 10);
 insert into voteTable value ("정연", (floor(rand()*9) + 1) * 10);
 insert into voteTable value ("모모", (floor(rand()*9) + 1) * 10);
@@ -98,83 +99,96 @@ insert into voteTable value ("미나", (floor(rand()*9) + 1) * 10);
 insert into voteTable value ("다현", (floor(rand()*9) + 1) * 10);
 insert into voteTable value ("채영", (floor(rand()*9) + 1) * 10);
 insert into voteTable value ("쯔위", (floor(rand()*9) + 1) * 10);
-select count(*) from voteTable;
-select * from voteTable;
 
-select name as 이름, count(name) as 득표수, (count(name) / (select count(*) from voteTable)) * 100 as 득표율 
-from voteTable group by name; # 9-1 실습
+select count(*) from voteTable; # 개수 세기
+select * from voteTable; # voteTable 전체 데이터 출력
 
+# 9-1 실습
+select name as 이름, count(name) as 득표수, (count(name) / (select count(*) from voteTable)) * 100 as 득표율
+from voteTable group by name; # name을 이름으로, count(name)을 득표수로, (count(name)/(select count(*) from voteTable))*100을 득표율로 출력
+
+# 9-2 실습
 select age as 연령대, count(name) as 득표수, count(name)/(select count(*) from voteTable where name='나연')*100 as 득표율
-from voteTable where name='나연' group by name, age;
+from voteTable where name='나연' group by name, age order by count(name) desc;
+# age를 연령대로, count(name)을 득표수로, count(name)/(select count(*) from voteTable where name='나연')*100을 득표율로 
+# 득표수를 기준으로 역순 출력
 
-select age as 연령대, count(age) as 득표수, count(age)/(select count(*) from voteTable where name="나연")*100 as 득표율 
-from voteTable where name='나연' group by age; # 9-2 실습
-select age as 연령대, count(name) as 득표수, (count(name) / (select count(*) from voteTable)) * 100 as 득표율 
-from voteTable group by name, age having name='정연';
+# 9-3 실습
+select age as 연령대, count(name) as 득표수, count(name)/(select count(*) from voteTable where name='정연')*100 as 득표율
+from voteTable where name='정연' group by name, age order by count(name) desc;
+# age를 연령대로, count(name)을 득표수로, count(name)/(select count(*) from voteTable where name='정연')*100을 득표율로,
+# 득표수를 기준으로 역순 출력
 
 -- procedure, function
-drop procedure if exists get_sum;
+drop procedure if exists get_sum; # 있으면 지워라
 delimiter $$
-create procedure get_sum(
-	in _id integer,
-    out _name varchar(20),
-    out _sum integer
+create procedure get_sum( # get_sum 프로시저 생성
+	in _id integer, # integer 변수 _id, 
+    # in : 프로시저에 값을 전달하며, 프로시저 내부에서 값을 수정할 수는 있지만 프로시저가 반환되고나서 호출자가 수정할 수는 없다.
+    out _name varchar(20), # varchar(20) _name 변수 생성
+    # out : 프로시저의 값을 호출자에게 다시 전달한다.
+    out _sum integer # integer _sum 변수 생성
 )
-begin
-	declare _kor integer;
-    declare _eng integer;
-    declare _mat integer;
+begin # 시작
+	declare _kor integer; # integer 변수 _kor 생성
+    declare _eng integer; # integer 변수 _eng 생성
+    declare _mat integer; # integer 변수 _mat 생성
     set _kor = 0; #로직상 의미는 없지만 declare / set으로 정의하고 값을 넣는다.
     
-    select name, kor, eng, mat
+    select name, kor, eng, mat 
 		into _name, _kor, _eng, _mat from examtable where id=_id;
+	# 변수 _name, _kor, _eng, _mat에 데이터 입력
     
-    set _sum = _kor + _eng + _mat;
-end $$
+    set _sum = _kor + _eng + _mat; # _sum = _kor+_eng_mat
+end $$ # delimiter 종료
 delimiter ;
 
 call get_sum(209905, @name, @sum); #프로시저 호출 가져오는 값은 @변수명 사용
 select @name, @sum; # 값 확인하는 방법
 
-drop function if exists f_get_sum;
+drop function if exists f_get_sum; # 있으면 지워라
 delimiter $$
-create function f_get_sum(_id integer) returns integer
-begin
-	declare _sum integer;
-    select kor+eng+mat into _sum from examtable where id=_id;
-return _sum;
-end $$
-
+create function f_get_sum(_id integer) returns integer # 함수 f_get_sum 생성
+begin # 시작
+	declare _sum integer; # integer 변수 _sum 생성
+    select kor+eng+mat into _sum from examtable where id=_id; # _sum에 kor+eng+mat 값 입력
+return _sum; # _sum 리턴
+end $$ # delimiter 종료
 delimiter ;
 
-select *, f_get_sum(id) from examtable;
+select *, f_get_sum(id) from examtable; # examtable 전체 데이터, f_get_sum(id) 값 출력
 
-drop procedure if exists insert_examtable;
+
+drop procedure if exists insert_examtable; # 있으면 지워라
 delimiter $$
-create procedure insert_examtable(_last integer)
-begin
-declare _name varchar(20);
-declare _id integer;
-declare _cnt integer;
-set _cnt=0;
-delete from examtable where id > 0;
-	_loop : loop
-		set _cnt = _cnt + 1;
+create procedure insert_examtable(_last integer) # insert_examtable 프로시저 생성
+begin # 시작
+declare _name varchar(20); # varchar(20) 변수 _name 생성
+declare _id integer; # integer 변수 _id 생성
+declare _cnt integer; # integer 변수 _cnt 생성
+set _cnt=0; # _cnt = 0으로 초기화
+delete from examtable where id > 0; # id가 1 이상인 데이터 전부 삭제
+	_loop : loop # 반복문
+		set _cnt = _cnt + 1; # _cnt++
         set _name = concat("홍길", cast(_cnt as char(4)));
+        # concat : 2개 이상의 문자열을 합치는 함수
+        # cast : 데이터 타입을 바꿔주는 함수 (= 형변환 함수)
         set _id = 209900 + _cnt;
+        # id = 209900 + _cnt
         
         insert into examtable value (_name, _id, rand()*100, rand()*100, rand()*100);
+        # examtable에 데이터 입력
         
-        if _cnt = _last then
-			leave _loop;
-		end if;
-	end loop _loop;
-end $$
+        if _cnt = _last then # _cnt가 입력받은 값과 같으면
+			leave _loop; # break
+		end if; # if문 종료
+	end loop _loop; # 반복문 종료
+end $$ # delimiter 종료
 delimiter ;
 
-call insert_examtable(1000);
-select * from examtable;
+call insert_examtable(1000); # insert_examtable에 1000 대입
+select * from examtable; # examtable 전체 데이터 출력
 
-select *, kor+eng+mat as sum, (kor+eng+mat)/3 as avg from examtable limit 30,59;
-desc voteTable;
+select *, kor+eng+mat as sum, (kor+eng+mat)/3 as avg from examtable limit 30,59; # examtable의 31번째 값부터 
+desc voteTable; 
 
