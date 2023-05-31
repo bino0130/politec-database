a1b067628405c8b6fa5ab1f0ce2a10c75c8dab73
bino <ska3287776@naver.com>
2023-05-31 22:00:53 +0900

    Update politec-database

--- 데이터베이스프로그래밍 코드/src/db/FreeWifi.java
+++ 데이터베이스프로그래밍 코드/src/db/FreeWifi.java
@@ -1,14 +1,12 @@
 package db;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
 
 public class FreeWifi {
 
